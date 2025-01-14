cafbc5cd5182fffa8364122adf38995be70fe3db
Alex Hagström <alex.hagstrom@student.kyh.se>
2025-01-15 00:48:53 +0100

    changed the code to make them work with unit testing easier

--- Personnummer.cs
+++ Personnummer.cs
@@ -1,22 +1,29 @@
-using System.Reflection.Metadata;
-
-internal class Personnummer
+public class Personnummer
 {
-    public string AddSeccurityNumber(string secNumb)
+    public string AddSecurityNumber(string secNumb)
     {
-        Console.Write("Please type in your security nymber...");
+        Console.Write("Please type in your security number...");
         secNumb = Console.ReadLine();
-        
-        int counter = secNumb.Length;
-        Console.WriteLine(counter);
-        
-        string birthDate = secNumb.Substring(0, 6);
-        string birthNumb = secNumb.Substring(6, 4);
-
-        secNumb = birthDate + "-" + birthNumb;
-
-        Console.WriteLine("Your security number is " + secNumb);
 
         return secNumb;
+    }
+
+    public int CheckSecurityNumber(string secNumb)
+    {
+        int count = secNumb.Length;
+
+        return count;
+    }
+
+    public string FormatSecurityNumber(string secNumb)
+    {
+        string yy = secNumb.Substring(0, 2);
+        string mm = secNumb.Substring(2, 2);
+        string dd = secNumb.Substring(4, 2);
+        string xxxx = secNumb.Substring(6, 4);
+
+        string formattedSecNumber = $"{yy}{mm}{dd}-{xxxx} ";
+    
+        return formattedSecNumber;
     }
 }
--- Tests.cs
+++ Tests.cs
@@ -1,10 +1,35 @@
-﻿namespace Grupp_7;
+﻿using System.Runtime.CompilerServices;
+using Xunit;
 
-public class UnitTest1
+public class PersonnummerTests
 {
     [Fact]
-    public void Test1()
+    public void TestCheck()
     {
+        //given
+        Personnummer per = new();
+        string secNumb = "9711138435";
+        int expectedResult = 10;
 
+        //when
+        int result = per.CheckSecurityNumber(secNumb);
+
+        //then
+        Assert.Equal(expectedResult, result);
+    }
+
+        [Fact]
+    public void FormatCheck()
+    {
+        //given
+        Personnummer per = new();
+        string secNumb = "9711138435";
+        string expectedResult = "971113-8435";
+
+        //when
+        string result = per.FormatSecurityNumber(secNumb);
+
+        //then
+        Assert.Equal(expectedResult, result);
     }
 }
