cb64db6529f0ee3f093cf4e8e4c60d137ed7ed45
KorpenM <magnuskorp@gmail.com>
2025-01-14 21:08:18 +0100

    Updated code to .cs

--- Personnummer.cs
+++ Personnummer.cs
@@ -1,22 +1,58 @@
-using System.Reflection.Metadata;
+using System.Collections.Generic;
+/******************************************************* 
+ UI
+*******************************************************/
 
-internal class Personnummer
+Console.Write("Please type in your security number...");
+string? secNumb = Console.ReadLine();
+
+if (CheckSecNumber(secNumb)!= null)
 {
-    public string AddSeccurityNumber(string secNumb)
-    {
-        Console.Write("Please type in your security nymber...");
-        secNumb = Console.ReadLine();
-        
-        int counter = secNumb.Length;
-        Console.WriteLine(counter);
-        
-        string birthDate = secNumb.Substring(0, 6);
-        string birthNumb = secNumb.Substring(6, 4);
-
-        secNumb = birthDate + "-" + birthNumb;
-
-        Console.WriteLine("Your security number is " + secNumb);
-
-        return secNumb;
-    }
+    System.Console.WriteLine(CheckSecNumber(secNumb));
 }
+else 
+{
+    Console.WriteLine("Value is null");
+}
+
+Console.WriteLine("Press any key to exit program...");
+Console.ReadKey();
+
+
+/******************************************************* 
+ Logic
+*******************************************************/
+
+
+//Check if the number is correct length (10 characters)
+string? CheckSecNumber(string secNumb)
+{
+    int counter = secNumb.Length;
+
+    if (counter == 10)
+    {
+        string formattedSecNumber = FormatSecNumber(secNumb);
+        return "Your security number is " + formattedSecNumber;
+
+    }
+    else if (secNumb.Length != 10)
+    {
+        return "Input is not valid";
+    }
+
+    return null;
+}
+
+//Format the number to yymmdd-xxxx
+string FormatSecNumber(string secNumb)
+{
+    string yy = secNumb.Substring(0, 2);
+    string mm = secNumb.Substring(2, 2);
+    string dd = secNumb.Substring(4, 2);
+    string xxxx = secNumb.Substring(6, 4);
+
+    string formattedSecNumber = $"{yy}{mm}{dd}-{xxxx} ";
+    
+    return formattedSecNumber;
+}
+
