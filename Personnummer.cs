74f93f705b13ceb80572d9998df058b7a106200f
Alex Hagström <alex.hagstrom@student.kyh.se>
2025-01-14 13:02:06 +0100

    changed previous code to turn it into a method

--- Personnummer.cs
+++ Personnummer.cs
@@ -1,22 +1,22 @@
-Console.Write("Please type in your security nymber...");
-string secNumb = Console.ReadLine();
+using System.Reflection.Metadata;
 
-int counter = secNumb.Length;
-Console.WriteLine(counter);
-
-if (counter == 10)
+internal class Personnummer
 {
-    string birthYear = secNumb.Substring(0, 2);
-    string birthMonth = secNumb.Substring(2, 2);
-    string birthDay = secNumb.Substring(4, 2);
-    string birthNumb = secNumb.Substring(6, 4);
+    public string AddSeccurityNumber(string secNumb)
+    {
+        Console.Write("Please type in your security nymber...");
+        secNumb = Console.ReadLine();
+        
+        int counter = secNumb.Length;
+        Console.WriteLine(counter);
+        
+        string birthDate = secNumb.Substring(0, 6);
+        string birthNumb = secNumb.Substring(6, 4);
 
-    Console.WriteLine("Your security number is " + birthYear + birthMonth + birthDay + "-" + birthNumb);
-}
-else if (counter != 10)
-{
-    Console.WriteLine("Invalid input");
-}
+        secNumb = birthDate + "-" + birthNumb;
 
-Console.WriteLine("Press any key to exit program...");
-Console.ReadKey();
+        Console.WriteLine("Your security number is " + secNumb);
+
+        return secNumb;
+    }
+}
